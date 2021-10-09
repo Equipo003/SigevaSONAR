d3c946abc6ca5af21bca1c0a3e2569cf80cfbdf0
SERGIO MOLINA LÓPEZ <Sergio.Molina10@alu.uclm.es>
2021-10-09 23:33:16 +0200

    Probando

---
+++ src/test/java/com/isoEquipo3/PracticaEquipo3/PracticaEquipo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isoEquipo3.PracticaEquipo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaEquipo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
