9e07370469dc4ad405b4fd96b8ca78d46ab93059
Sergio Martín <sergio.martinlgado@alu.uclm.es>
2021-10-16 16:34:42 +0200

    Creada estructura proyecto

---
+++ SiGEVA/SIGEVA-Service/src/main/java/com/isoEquipo3/SIGEVA/App.java
@@ -0,0 +1,12 @@
+package com.isoEquipo3.SIGEVA;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
