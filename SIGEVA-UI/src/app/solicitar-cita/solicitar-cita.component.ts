21cedef030e658545362a0b1755c374ff3eeffa3
JoseMiguelMartinezMalonda <JoseMiguel.Martinez8@alu.uclm.es>
2021-10-30 20:34:00 +0200

    IU Solicitar Cita

---
+++ SIGEVA-UI/src/app/solicitar-cita/solicitar-cita.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import {Usuario} from "../Model/Usuario";
+import {CentroSalud} from "../Model/centro-salud";
+import {CupoCitas} from "../Model/cupo-citas";
+import { JsonService } from '../Service/json.service';
+
+
+
+@Component({
+  selector: 'app-solicitar-cita',
+  templateUrl: './solicitar-cita.component.html',
+  styleUrls: ['./solicitar-cita.component.css']
+})
+export class SolicitarCitaComponent implements OnInit {
+  paciente : Usuario;
+  mensaje: string;
+  cita1 : CupoCitas;
+  cita2 : CupoCitas;
+  citas : CupoCitas[];
+  solicitada : boolean;
+;
+
+  constructor(private json: JsonService) {
+    this.paciente = new Usuario("","", "", "", "", "",
+          "", "", "", "");
+    this.mensaje = "";
+    this.cita1 = new CupoCitas("",new CentroSalud("","",0,""), new Date());
+    this.cita2 = new CupoCitas("",new CentroSalud("","",0,""), new Date());
+    this.citas = [];
+    this.solicitada = false;
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  solicitarCita(){
+    let centroSalud = new CentroSalud("Calle mi calle", "Hospital Universitario", 1340, "8071ef83-2230-40b5-bb4e-9b3167006f8f");
+     this.json.postJson("cupo/buscarParDeCuposLibresAPartirDeHoy",centroSalud).subscribe(
+          result => {
+
+          this.citas = JSON.parse(result.toString());
+          this.cita1 = this.citas[0];
+          this.cita2 = this.citas[1];
+          this.mensaje = 'CITA RESERVADA!'
+          },err=> {
+            this.mensaje = err.error.message;
+            console.log(err);
+          });
+     this.solicitada = true;
+
+  }
+
+}
