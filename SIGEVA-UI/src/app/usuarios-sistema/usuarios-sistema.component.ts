2c592a9263237b0bfaa2f2c1630a63f722f4506b
Julio.Molina <julio.molina@alu.uclm.es>
2021-10-31 18:13:15 +0100

    --Interfaz listar Usuarios acabada

--- SIGEVA-UI/src/app/usuarios-sistema/usuarios-sistema.component.ts
+++ SIGEVA-UI/src/app/usuarios-sistema/usuarios-sistema.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../Model/Usuario';
 import { JsonService } from '../Service/json.service';
-import {HttpParams} from "@angular/common/http";
+import { HttpParams } from "@angular/common/http";
+import { CentroSalud } from '../Model/centro-salud';
+import { Rol } from '../Model/rol';
 
 @Component({
 	selector: 'app-usuarios-sistema',
@@ -10,12 +12,38 @@ import {HttpParams} from "@angular/common/http";
 })
 export class UsuariosSistemaComponent implements OnInit {
 	usuarios: Usuario[];
+	cs: CentroSalud[];
+	roles: Rol[];
 
 	constructor(private json: JsonService) {
 		this.usuarios = [];
+		this.cs = [];
+		this.roles = [];
 	}
 	ngOnInit(): void {
+		this.cargarCentros();
+		this.cargarRoles();
 		this.cargarUsuarios();
+	}
+
+
+	cargarRoles() {
+		this.json.getJson("user/getRoles").subscribe(
+			(res: any) => {
+				this.roles = JSON.parse(res);
+			},
+			error => {
+				console.log(error);
+			}
+		);
+	}
+	cargarCentros() {
+		this.json.getJson('user/getCentros').subscribe(
+			(res: any) => {
+				this.cs = JSON.parse(res);
+			}, error => {
+				console.log(error);
+			});
 	}
 
 	cargarUsuarios() {
@@ -26,13 +54,23 @@ export class UsuariosSistemaComponent implements OnInit {
 		});
 		let self = this;
 		this.json.getJsonP("user/getUsuariosByRol", params).subscribe(
-			(res : any)=>{
+			(res: any) => {
 				this.usuarios = JSON.parse(res);
 				console.log(res);
-			},
-			(error : any)=>{
+				this.cs.forEach(function(centro2: CentroSalud) {
+					self.usuarios.forEach(function(usuario: Usuario) {
+						self.roles.forEach(function(rol: Rol) {
+							if (usuario.centroSalud === centro2.id) {
+								usuario.centroSalud = centro2.nombreCentro;
+							};
+							if (usuario.rol === rol.id) {
+								usuario.rol = rol.nombre;
+							}
+						});
+					});
+				});
+			}, error => {
 				console.log(error);
-			}
-			);
+			});
 	}
 }
