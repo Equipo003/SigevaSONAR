7f1516e0da1b1190cf178475ee0163a5da738978
Julio.Molina <julio.molina@alu.uclm.es>
2021-11-02 23:01:24 +0100

    --Otro test más FijarUsuarioCentroSalud

--- SIGEVA-Service/src/test/java/com/equipo3/SIGEVA/FijarUsuarioCentroSalud.java
+++ SIGEVA-Service/src/test/java/com/equipo3/SIGEVA/FijarUsuarioCentroSalud.java
@@ -3,6 +3,7 @@ package com.equipo3.SIGEVA;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,7 @@ import com.equipo3.SIGEVA.dao.CentroSaludDao;
 import com.equipo3.SIGEVA.dao.RolDao;
 import com.equipo3.SIGEVA.dao.UsuarioDao;
 import com.equipo3.SIGEVA.model.CentroSalud;
+import com.equipo3.SIGEVA.model.Rol;
 import com.equipo3.SIGEVA.model.Sanitario;
 import com.equipo3.SIGEVA.model.Usuario;
 
@@ -32,10 +34,38 @@ class FijarUsuarioCentroSalud {
 	void AsignarCentroSaludSanitarioNivelModelo() {
 		Usuario sanitario = new Sanitario();
 		CentroSalud cs = new CentroSalud();
+		cs.setNombreCentro(UUID.randomUUID().toString());
+		administradorController.crearCentroSalud(cs);
+		
 		sanitario.setNombre("NombrePrueba");
-		sanitario.setRol("e24bf973-e26e-47b7-b8f4-83fa13968221");
-		sanitario.setCentroSalud(cs.getId());
-		Assertions.assertEquals(sanitario.getCentroSalud(), cs.getId());
+		sanitario.setUsername(UUID.randomUUID().toString());
+		Rol rol = null;
+		
+		if(roldao.findAllByNombre("Sanitario").isPresent()) {
+			rol = roldao.findAllByNombre("Sanitario").get();
+		}
+		
+		sanitario.setRol(rol.getId());
+		System.out.println("Rol: " +rol.getId());
+		
+		if(sanitario!=null) {
+			System.out.println("No es nulo");
+			System.out.println(sanitario.getRol());
+		}
+		
+		administradorController.fijarPersonal(sanitario.getUsername(), cs.getId());
+		
+		Usuario san = null;
+		
+		administradorController.crearUsuarioSanitario((Sanitario) sanitario);
+		
+		System.out.println("Id Sanitario "+sanitario.getIdUsuario());
+		
+		if(usuarioDao.findById(sanitario.getIdUsuario()).isPresent()) {
+			san = usuarioDao.findById(sanitario.getIdUsuario()).get();
+			System.out.println("Esta presente");
+		}
+		Assertions.assertEquals(sanitario.getCentroSalud(), san.getCentroSalud());
 	}
 
 	@Test
