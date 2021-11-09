5b1af6911584c477da44de43939142d24e7e70d0
SERGIO MOLINA LÓPEZ <Sergio.Molina10@alu.uclm.es>
2021-11-09 18:04:43 +0100

    Eliminar centro de salud conseguido

--- SIGEVA-UI/src/app/centro-salud/centro-salud.component.ts
+++ SIGEVA-UI/src/app/centro-salud/centro-salud.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CentroSalud } from '../Model/centro-salud';
-
+import {JsonService} from "../Service/json.service";
 @Component({
   selector: 'app-centro-salud',
   templateUrl: './centro-salud.component.html',
@@ -8,13 +8,31 @@ import { CentroSalud } from '../Model/centro-salud';
 })
 export class CentroSaludComponent implements OnInit {
   @Input() cs: CentroSalud;
+  idCentro: string;
+  public message: string;
+  public errorMessage: string;
 
-  constructor() {
+  constructor(private json: JsonService) {
 	this.cs = new CentroSalud("", "", 0);
+	this.idCentro="";
+	this.errorMessage = "";
+    this.message = "";
   }
 
   ngOnInit(): void {
 	
   }
-
+  enviarDatosBack() {
+   
+      let self = this;
+      this.json.postJson("user/deleteCentroSalud", this.cs).subscribe(
+        result => {
+          this.message = "Centro eliminado correctamente.";
+          setTimeout(function(){ window.location.reload(); }, 3000);
+        },err=> {
+          this.errorMessage = err.error.message;
+          console.log(err);
+        });
+    
+  }
 }
