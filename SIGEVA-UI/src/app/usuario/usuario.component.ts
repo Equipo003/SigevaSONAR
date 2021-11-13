fc573e1084b787c44053ae04b72699a6e583f714
Sergio Martín <sergio.martinlgado@alu.uclm.es>
2021-11-13 10:47:14 +0100

    Añadido boton eliminar usuario

--- SIGEVA-UI/src/app/usuario/usuario.component.ts
+++ SIGEVA-UI/src/app/usuario/usuario.component.ts
@@ -2,6 +2,10 @@ import { Component, Input } from '@angular/core';
 import {UsuarioConObjetos} from "../Model/Usuario-con-objetos";
 import {CentroSalud} from "../Model/centro-salud";
 import {Rol} from "../Model/rol";
+import {VentanaEmergenteComponent} from "../ventana-emergente/ventana-emergente.component";
+import {MatDialog} from "@angular/material/dialog";
+import {JsonService} from "../Service/json.service";
+import {Usuario} from "../Model/Usuario";
 
 @Component({
 	selector: 'app-usuario',
@@ -11,9 +15,31 @@ import {Rol} from "../Model/rol";
 
 export class UsuarioComponent {
 	@Input() usuario: UsuarioConObjetos;
-	constructor() {
+  message: string = "";
+  errorMessage: string = "";
+	constructor(private json: JsonService, public dialog: MatDialog) {
 		this.usuario = new UsuarioConObjetos(new Rol("", ""), new CentroSalud("", "",
       0), "", "", "", "", "", "", "",
       "");
 	}
+
+  openDialogEliminar() {
+    let self = this;
+    const dialogRef = this.dialog.open(VentanaEmergenteComponent, {
+      data: {mensaje: '¿SEGURO QUE QUIERES ELIMINAR AL USUARIO: ' + this.usuario.nombre + ' ' + this.usuario.apellidos + '?', titulo: 'Eliminar Usuario'},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.json.postJson("user/removeUsuario", this.usuario).subscribe(
+          result => {
+            this.message = "Usuario eliminado correctamente";
+            setTimeout(function(){ self.message= ""; self.errorMessage = "" }, 3000);
+            this.errorMessage = "";
+          }, error => {
+            this.errorMessage = error.error.message;
+            this.message = "";
+          });
+      }
+    });
+  }
 }
