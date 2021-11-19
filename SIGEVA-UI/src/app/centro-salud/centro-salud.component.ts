319e2c78c06100350d36ecf6ca95bb4edceae869
Sergio Martín <sergio.martinlgado@alu.uclm.es>
2021-11-19 21:04:18 +0100

    Corregidos iconos ver centros

--- SIGEVA-UI/src/app/centro-salud/centro-salud.component.ts
+++ SIGEVA-UI/src/app/centro-salud/centro-salud.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
-import { CentroSalud } from '../Model/centro-salud';
+import {CentroSalud} from '../Model/centro-salud';
 import {JsonService} from "../Service/json.service";
-import { VentanaEmergenteComponent } from '../ventana-emergente/ventana-emergente.component';
+import {VentanaEmergenteComponent} from '../ventana-emergente/ventana-emergente.component';
 import {Vacuna} from "../Model/vacuna";
+
 @Component({
   selector: 'app-centro-salud',
   templateUrl: './centro-salud.component.html',
@@ -15,47 +16,53 @@ export class CentroSaludComponent implements OnInit {
   idCentro: string;
   public message: string;
   public errorMessage: string;
+  centroEliminado = false;
 
-  constructor( public dialog: MatDialog,private json: JsonService) {
-	this.cs = new CentroSalud("direccion", "nombre",1, new Vacuna("vacuna", 3, 15), "");
-	this.idCentro="";
-	this.errorMessage = "";
+  constructor(public dialog: MatDialog, private json: JsonService) {
+    this.cs = new CentroSalud("direccion", "nombre", 1, new Vacuna("vacuna", 3, 15), "");
+    this.idCentro = "";
+    this.errorMessage = "";
     this.message = "";
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
-   openDialog() {
-	   let self = this;
-       const dialogRef = this.dialog.open(VentanaEmergenteComponent, {
-         data: { mensaje: '¿SEGURO QUE QUIERES ELIMINAR EL CENTRO?', titulo: 'Eliminar centro de salud'},
-       });
-       dialogRef.afterClosed().subscribe(result => {
-		 if(result){
-			this.json.postJson("user/deleteCentroSalud", this.cs).subscribe(
-        result => {
-          this.message = "Centro eliminado correctamente.";
-          setTimeout(function(){ window.location.reload(); }, 3000);
-        },err=> {
-          this.errorMessage = err.error.message;
- 		  setTimeout(function(){ self.errorMessage=""; }, 3000);
-        });
-		this.json.postJson("cupo/borrarCuposDelCentro", this.cs).subscribe(
-        result => {
-          this.message = "";
-         // setTimeout(function(){ window.location.reload(); }, 3000);
-        },err=> {
-          this.errorMessage = err.error.message;
- 		 // setTimeout(function(){ self.errorMessage=""; }, 3000);
-        });
+  openDialog() {
+    let self = this;
+    const dialogRef = this.dialog.open(VentanaEmergenteComponent, {
+      data: {mensaje: '¿SEGURO QUE QUIERES ELIMINAR EL CENTRO?', titulo: 'Eliminar centro de salud'},
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+
+        this.json.postJson("user/deleteCentroSalud", this.cs).subscribe(
+          result => {
+            this.message = "Centro eliminado correctamente.";
+            this.centroEliminado = true;
+          }, err => {
+            this.errorMessage = err.error.message;
+            setTimeout(function () {
+              self.errorMessage = "";
+            }, 3000);
+          });
 
 
-		}
-       });
-     }
+        this.json.postJson("cupo/borrarCuposDelCentro", this.cs).subscribe(
+          result => {
+            this.message = "";
+            // setTimeout(function(){ window.location.reload(); }, 3000);
+          }, err => {
+            this.errorMessage = err.error.message;
+            // setTimeout(function(){ self.errorMessage=""; }, 3000);
+          });
+      }
+    });
+  }
+
   enviarDatosBack() {
 
-      let self = this;
-	  this.openDialog();
+    let self = this;
+    this.openDialog();
   }
 }
