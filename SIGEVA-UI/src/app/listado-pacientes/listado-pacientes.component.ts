087b4e18341cf281e40523697c7769f0605d3869
JoseMiguelMartinezMalonda <JoseMiguel.Martinez8@alu.uclm.es>
2021-11-20 18:14:28 +0100

    Interfaz listado pacientes trabaja con localStorage

--- SIGEVA-UI/src/app/listado-pacientes/listado-pacientes.component.ts
+++ SIGEVA-UI/src/app/listado-pacientes/listado-pacientes.component.ts
@@ -10,6 +10,8 @@ import {MatDatepickerInputEvent} from '@angular/material/datepicker';
 import {Vacuna} from "../Model/vacuna";
 import {CupoCitas} from "../Model/cupo-citas";
 import {FormControl} from "@angular/forms";
+import {TokenService} from "../Service/token.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-listado-pacientes',
@@ -17,83 +19,36 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./listado-pacientes.component.css']
 })
 export class ListadoPacientesComponent implements OnInit {
-  pacientes : Usuario[];
-  roles : Rol[];
   citaSeleccionada:CitaConObjetos;
   citas : CitaConObjetos[];
-  pacienteCentroSalud : CentroSalud;
   pacienteSeleccionado:boolean;
   today : FormControl;
   dateSelectedIsToday : boolean;
+  centroSaludUsuario: CentroSalud;
 
-  constructor(private json:JsonService) {
-    this.pacientes = [];
-    this.roles = [];
-    this.pacienteCentroSalud = new CentroSalud("direccion", "nombre",1, new Vacuna("vacuna", 3, 15), "");
-    this.citaSeleccionada = new CitaConObjetos(new CupoCitas("",this.pacienteCentroSalud, new Date() ), 0, new Paciente(new Rol("1", "Paciente"),new CentroSalud("direccion", "nombre",1, new Vacuna("vacuna", 3, 15), ""), "vasilesan", "", "", "",
+  constructor(private json:JsonService, private tokenService: TokenService, private router: Router) {
+    this.citaSeleccionada = new CitaConObjetos(new CupoCitas("",new CentroSalud("direccion", "nombre",0, new Vacuna("vacuna", 0, 0), ""), new Date() ), 0, new Paciente(new Rol("0", ""),new CentroSalud("direccion", "nombre",1, new Vacuna("vacuna", 0, 0), ""), "", "", "", "",
       "", "", "", "", 0));
+    this.centroSaludUsuario = new CentroSalud("direccion", "nombre",0, new Vacuna("vacuna", 0, 0), "")
     this.citas = [];
-    this.pacienteSeleccionado = false;
     this.today = new FormControl(new Date());
+    this.pacienteSeleccionado = false;
     this.dateSelectedIsToday = true;
   }
 
   ngOnInit(): void {
-    //this.getRoles();
-    this.getPacientePrueba();
-    this.citasHoy();
-  }
-
-  getRoles() {
-    let self = this;
-    this.json.getJson("user/getRoles").subscribe(
-      result => {
-        this.roles = JSON.parse(result);
-        this.getRolPaciente();
-      }, error => {
-        console.log(error);
-      });
-  }
-
-  getRolPaciente() {
-    let self = this;
-    this.roles.forEach(function(rol: Rol) {
-      if (rol.nombre === "Sanitario") {
-        self.getParams(rol);
-      }
-    })
-  }
-
-  getParams(rolAux: Rol) {
-    let params = new HttpParams({
-      fromObject: {
-        rol: rolAux.id,
-      }
-    });
-    this.json.getJsonP("user/getUsuariosByRol/", params).subscribe(
-      result => {
-        this.pacientes = JSON.parse(result)
-      }, error => {
-        console.log(error);
-      });
+    if (this.tokenService.getToken() == null) {
+      this.router.navigate(['/login']);
+    }
+    else {
+      this.getCentroSaludUsuario(this.tokenService.getIdUsuario());
+     // this.citasHoy();
+    }
   }
 
   vacunar(cita : CitaConObjetos){
-
     this.citaSeleccionada = cita;
     this.pacienteSeleccionado = true;
-  }
-
-
-  getPacientePrueba(){
-    this.json.getJson("cita/getPacientePrueba").subscribe(
-      result => {
-        let paciente : Paciente;
-        paciente = JSON.parse(result);
-        this.pacienteCentroSalud = paciente.centroSalud;
-      }, error => {
-        console.log(error);
-      });
   }
 
   dataChangeEvent(type: string, event: MatDatepickerInputEvent<Date>) {
@@ -106,7 +61,7 @@ export class ListadoPacientesComponent implements OnInit {
   getPacientesFecha(event : MatDatepickerInputEvent<Date>){
     let params = new HttpParams({
       fromObject: {
-        'centroSaludDTO': JSON.stringify(this.pacienteCentroSalud),
+        'centroSaludDTO': JSON.stringify(this.centroSaludUsuario),
         'fecha' : JSON.stringify(event.value),
       }
     });
@@ -131,13 +86,31 @@ export class ListadoPacientesComponent implements OnInit {
     this.today = new FormControl(new Date());
     let params = new HttpParams({
       fromObject: {
-        'centroSaludDTO': JSON.stringify(this.pacienteCentroSalud),
+        'centroSaludDTO': JSON.stringify(this.centroSaludUsuario),
         'fecha' : JSON.stringify(new Date()),
       }
     });
     this.json.getJsonP("cita/obtenerCitasFecha", params).subscribe(
       result => {
         this.citas = JSON.parse(result);
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  private getCentroSaludUsuario(idUsuario: string | null) {
+    if(idUsuario == null){
+      idUsuario = "";
+    }
+
+    let params = new HttpParams({
+      fromObject: {
+        'idUsuario': idUsuario,
+      }
+    });
+    this.json.getJsonP("user/getCentroSanitario", params).subscribe(
+      result => {
+        this.centroSaludUsuario = JSON.parse(result);
       }, error => {
         console.log(error);
       });
