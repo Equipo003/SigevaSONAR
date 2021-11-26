2b3aaa73ac2370e61d39aeb86212532d98e2c53f
JoseMiguelMartinezMalonda <JoseMiguel.Martinez8@alu.uclm.es>
2021-11-26 21:27:27 +0100

    Sonarlint solved

--- SIGEVA-Service/src/main/java/com/equipo3/SIGEVA/controller/ViewController.java
+++ SIGEVA-Service/src/main/java/com/equipo3/SIGEVA/controller/ViewController.java
@@ -1,7 +1,9 @@
 package com.equipo3.SIGEVA.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /***
  * Controlador para mapear las peticiones del front end en el back end.
@@ -12,9 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-	@RequestMapping({ "/home", "/crearCS", "/cnfgCupos", "/crearUsuarios", "/indicarDosisVacunas", "/usuariosSistema",
+	@RequestMapping(path={ "/home", "/crearCS", "/cnfgCupos", "/crearUsuarios", "/indicarDosisVacunas", "/usuariosSistema",
 			"/solicitarCita", "/editarUsuario/:idUsuario", "/editarCS/:idCentroSalud", "/listarPacientes", "/login",
-			"/misCitas" })
+			"/misCitas" }, method = {RequestMethod.GET})
 	public String index() {
 		return "forward:/index.html";
 	}
